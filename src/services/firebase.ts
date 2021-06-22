25652d5ebb4f66629e898a21a6204d9b80a54bb4
Lucas Torquato <front.end.lb@gmail.com>
2021-06-22 19:30:34 -0300

    dia 02 finalizado

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,7 +3,6 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -16,5 +15,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database }
